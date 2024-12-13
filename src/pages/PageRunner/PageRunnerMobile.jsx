68a762d7d024a59f3b7e80215668b7b8fdbc100f
posadsky84 <a.i.posadsky@gmail.com>
2024-12-13 22:08:00 +0300

    Профиль участника. Верстка + раскрывающиеся детали

--- src/pages/PageRunner/PageRunnerMobile.jsx
+++ src/pages/PageRunner/PageRunnerMobile.jsx
@@ -5,8 +5,72 @@ import { Link } from 'react-router-dom';
 import Reward from '../../components/Reward/Reward';
 import dayjs from 'dayjs';
 import { resultToStr } from '../../helper';
+import { useState } from 'react';
+
+const RunnerCardMobile = ({teamContent, distanceContent, raceContent, runner}) => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  return <>
+    <div className="run-card-yr-mobile">
+
+    </div>
+    <div className="run-card-mobile" onClick={() => setIsOpened(!isOpened)}>
+      <div className="run-card-caption-mobile">
+        <div className="run-card-distance-mobile">
+          {raceContent.sname}
+          <CourseSticker type={distanceContent.courseType} value={distanceContent.km}/>
+        </div>
+        <div className="run-card-results-mobile">
+          Место: <div className="run-card-place-mobile">{teamContent.place}</div>
+          <Reward label={teamContent.reward}/>
+        </div>
+      </div>
+      <div className="run-card-detail-mobile">
+        <div className="cell-item">
+          <div className="run-card-team-name-mobile">{teamContent.name}</div>
+          {!!teamContent.dns && <DnsLabel/>}
+          {!!teamContent.dnf && <DnfLabel/>}
+        </div>
+        {
+          teamContent.members.data.map(coMemberItem => {
+            let strRunner = `${coMemberItem.attributes.runner.data.attributes.lastName ? coMemberItem.attributes.runner.data.attributes.lastName : ""}`;
+            strRunner += `${coMemberItem.attributes.runner.data.attributes.firstName ? " " + coMemberItem.attributes.runner.data.attributes.firstName : ""}`;
+
+            if (coMemberItem.attributes.runner.data.id === runner.data.id) {
+              return (<div className="run-card-member-mobile">
+                <div className={`${coMemberItem.attributes.dns || teamContent.dns ? "dns" : ""}`}>
+                  {strRunner}
+                </div>
+                {!!coMemberItem.attributes.dns && <DnsLabel/>}
+                {!!coMemberItem.attributes.dnf && <DnfLabel/>}
+              </div>);
+            } else {
+              return (<div className="run-card-member-mobile">
+                <Link
+                  className={`runner-link ${coMemberItem.attributes.dns || teamContent.dns ? "dns" : ""}`}
+                  to={`/runners/${coMemberItem.attributes.runner.data.id}`}>
+                  {strRunner}
+                </Link>
+                {!!coMemberItem.attributes.dns && <DnsLabel/>}
+                {!!coMemberItem.attributes.dnf && <DnfLabel/>}
+              </div>);
+            }
+          })
+        }
+
+
+      </div>
+    </div>
+    {isOpened && <div className="run-card-footer-mobile">
+      {teamContent.start ? dayjs(teamContent.start).format('DD.MM.YYYY HH:mm') : ""}
+      {teamContent.finish ? dayjs(teamContent.finish).format('DD.MM.YYYY HH:mm') : ""}
+      {resultToStr(teamContent.result)}
+    </div>}
+  </>;
+}
 
 const PageRunnerMobile = ({teamsData, runner}) => {
+
   return teamsData.toSorted((a, b) => {
     const ddate1 = new Date(a.attributes.team.data.attributes.distance.data.attributes.race.data.attributes.ddate);
     const ddate2 = new Date(b.attributes.team.data.attributes.distance.data.attributes.race.data.attributes.ddate);
@@ -16,63 +80,11 @@ const PageRunnerMobile = ({teamsData, runner}) => {
     const distanceContent = teamContent.distance.data.attributes;
     const raceContent = distanceContent.race.data.attributes;
 
-    return <>
-      <div className="run-card-yr-mobile">
-
-      </div>
-      <div className="run-card-mobile">
-        <div className="run-card-caption-mobile">
-          {raceContent.name}
-          <div className="cell-item">
-            <div className={teamContent.dns ? "dns" : ""}>{teamContent.name}</div>
-            {!!teamContent.dns && <DnsLabel/>}
-            {!!teamContent.dnf && <DnfLabel/>}
-          </div>
-          <CourseSticker type={distanceContent.courseType} value={distanceContent.km}/>
-          {teamContent.place}
-          <Reward label={teamContent.reward}/>
-        </div>
-        <div className="run-card-detail-mobile">
-
-          {
-            teamContent.members.data.map(coMemberItem => {
-              let strRunner = `${coMemberItem.attributes.runner.data.attributes.lastName ? coMemberItem.attributes.runner.data.attributes.lastName : ""}`;
-              strRunner += `${coMemberItem.attributes.runner.data.attributes.firstName ? " " + coMemberItem.attributes.runner.data.attributes.firstName : ""}`;
-
-              if (coMemberItem.attributes.runner.data.id === runner.data.id) {
-                return (<div className="cell-item">
-                  <div className={`${coMemberItem.attributes.dns || teamContent.dns ? "dns" : ""}`}>
-                    {strRunner}
-                  </div>
-                  {!!coMemberItem.attributes.dns && <DnsLabel/>}
-                  {!!coMemberItem.attributes.dnf && <DnfLabel/>}
-                </div>);
-              } else {
-                return (<div className="cell-item">
-                  <Link
-                    className={`runner-link ${coMemberItem.attributes.dns || teamContent.dns ? "dns" : ""}`}
-                    to={`/runners/${coMemberItem.attributes.runner.data.id}`}>
-                    {strRunner}
-                  </Link>
-                  {!!coMemberItem.attributes.dns && <DnsLabel/>}
-                  {!!coMemberItem.attributes.dnf && <DnfLabel/>}
-                </div>);
-              }
-            })
-          }
-
-
-        </div>
-      </div>
-      <div className="run-card-footer-mobile">
-        {teamContent.start ? dayjs(teamContent.start).format('DD.MM.YYYY HH:mm') : ""}
-        {teamContent.finish ? dayjs(teamContent.finish).format('DD.MM.YYYY HH:mm') : ""}
-        {resultToStr(teamContent.result)}
-      </div>
-    </>;
-
-
-
+    return <RunnerCardMobile teamContent={teamContent}
+                             distanceContent={distanceContent}
+                             raceContent={raceContent}
+                             runner={runner}
+                             />;
 
   });
 
