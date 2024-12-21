b2e833912a970f4b2faffd334df0e57d71476824
posadsky84 <a.i.posadsky@gmail.com>
2024-12-21 14:23:21 +0300

    Все гонки - новая верстка + мобил.верстка

--- src/pages/PageAllRaces/PageAllRaces.jsx
+++ src/pages/PageAllRaces/PageAllRaces.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useGetRacesQuery } from '../../redux/baseApi';
-import './pageAllRaces.css';
+import './pageAllRaces.scss';
 import { getStrapiImageUrl } from '../../helper';
 import { Link } from 'react-router-dom';
 import CourseSticker from '../../components/CourseSticker/CourseSticker';
@@ -28,48 +28,54 @@ const PageAllRaces = () => {
   } else if (isSuccess) {
     let curYear = 0;
     racesContent = allRaces.data.map(raceItem => {
-      let divider;
-      const itemYear = new Date(raceItem.attributes.ddate).getFullYear();
-      if (curYear !== itemYear) {
-        curYear = itemYear;
-        divider = (<div className="year-divider">Турмарафон {itemYear}</div>);
-      }
+        let divider;
+        const itemYear = new Date(raceItem.attributes.ddate).getFullYear();
+        if (curYear !== itemYear) {
+          curYear = itemYear;
+          divider = (<>
+            <div className="year-divider-new">Турмарафон {itemYear}</div>
+            <div className="year-divider-new">Дистанции</div>
+            <div className="year-divider-new">Участников</div>
+          </>);
+        }
 
       let courseStickers = [];
-      let startedCounts = [];
-      let totalStartedCount = 0;
-      raceItem.attributes.distances.data.toSorted((a, b) => {
-        return a.attributes.km < b.attributes.km ? -1 : 1
-      }).forEach(distanceItem => {
-        courseStickers.push((<Link className="distance-link" to={`/races/${raceItem.id}?distance=${distanceItem.attributes.courseType}`}>
-          <CourseSticker type={distanceItem.attributes.courseType} value={distanceItem.attributes.km} />
-        </Link>));
-        totalStartedCount += distanceItem.attributes.runnersStartedCount;
-        startedCounts.push({type: distanceItem.attributes.courseType, value: distanceItem.attributes.runnersStartedCount});
-      });
+        let startedCounts = [];
+        let totalStartedCount = 0;
+        raceItem.attributes.distances.data.toSorted((a, b) => {
+          return a.attributes.km < b.attributes.km ? -1 : 1
+        }).forEach(distanceItem => {
+          courseStickers.push((<Link className="distance-link" to={`/races/${raceItem.id}?distance=${distanceItem.attributes.courseType}`}>
+            <CourseSticker type={distanceItem.attributes.courseType} value={distanceItem.attributes.km} />
+          </Link>));
+          totalStartedCount += distanceItem.attributes.runnersStartedCount;
+          startedCounts.push({type: distanceItem.attributes.courseType, value: distanceItem.attributes.runnersStartedCount});
+        });
 
 
-      return <>
-        {divider}
-        <div className="all-races-cell caption-cell">
-          <img className="magnet-image" alt="" src={getStrapiImageUrl(raceItem.attributes.magnet.data?.attributes.url)}></img>
-          <div className="race-caption">
-            <Link className="distance-link" to={`/races/${raceItem.id}`}>{raceItem.attributes.sname}</Link>
-            <div className="race-info">{raceItem.attributes.ddate}, {raceItem.attributes.location}</div>
+        return <>
+          {divider}
+          <div className="all-races-cell caption-cell">
+            <img className="magnet-image" alt=""
+                 src={getStrapiImageUrl(raceItem.attributes.magnet.data?.attributes.url)}></img>
+            <div className="race-caption">
+              <Link className="distance-link" to={`/races/${raceItem.id}`}>{raceItem.attributes.sname}</Link>
+              <div className="race-info">{raceItem.attributes.ddate}, {raceItem.attributes.location}</div>
+            </div>
           </div>
-          <div className="distances-cell">
+          <div className="all-races-distances-cell">
             {courseStickers}
           </div>
-        </div>
-        <div className="all-races-cell distances-cell">
+          <div className="all-races-cell distances-cell">
             {!!totalStartedCount && (
               <p>
-              <span className="cnt-span">{totalStartedCount} (</span>
-              {startedCounts.map((item, index) => <><span>{index ? ` / ` : ``}</span><span className={`cnt-span ${item.type}`}>{item.value}</span></>)}
-              )</p>
-              )}
-        </div>
-      </>;
+                <span className="cnt-span">{totalStartedCount} (</span>
+                {startedCounts.map((item, index) => <><span>{index ? ` / ` : ``}</span><span
+                  className={`cnt-span ${item.type}`}>{item.value}</span></>)}
+                )</p>
+            )}
+          </div>
+        </>;
     });
   } else if (isError) {
     racesContent = (
@@ -80,11 +86,12 @@ const PageAllRaces = () => {
   }
 
 
-  return (
-    <div className="all-races-table">
+  return (<>
+      <div className="all-races-grid">
       {racesContent}
-    </div>
-  );
+      </div>
+    </>
+    );
 };
 
 export default PageAllRaces;
