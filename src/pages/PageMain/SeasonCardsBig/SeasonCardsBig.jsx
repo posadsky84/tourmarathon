d3b7def924d43d4476bfea3adf2449477ef1de87
posadsky84 <a.i.posadsky@gmail.com>
2025-05-28 23:30:31 +0300

    Изменения верстки главной страницы

--- src/pages/PageMain/SeasonCardsBig/SeasonCardsBig.jsx
+++ src/pages/PageMain/SeasonCardsBig/SeasonCardsBig.jsx
@@ -7,18 +7,31 @@ const SeasonCardsBig = ({data}) => {
 
   return (<>
     {data.data.map(card => {
-      return <div className={`tm-card ${card.attributes.status === raceStatus.opened ? "actual" : ""}`}>
-        <img className="tm-card-image" alt="" src={getStrapiImageUrl(card.attributes.cardPicture.data?.attributes.url)}></img>
-        <div className="tm-card-caption">{card.attributes.name}</div>
-        <div className="tm-card-ddate">{toFineDate(new Date(card.attributes.ddate))}</div>
-        <div className="tm-card-distances">
-          {card.attributes.distances.data.toSorted((a, b) => {
-            return a.attributes.km < b.attributes.km ? -1 : 1
-          }).map(item => {
-            return <CourseSticker type={item.attributes.courseType} value={item.attributes.km}/>
-          })}
+      return (
+        <div className={`tm-card ${!card.attributes.status ? "blocked" : ""}`}>
+          <div className="tm-card-image-wrapper">
+            <img className="tm-card-image" alt=""
+               src={getStrapiImageUrl(card.attributes.cardPicture.data?.attributes.url)}></img>
+            {card.attributes.status === raceStatus.opened && (
+              <div className="tm-card-registration-button">Регистрация</div>
+            )}
+          </div>
+          <div className="tm-card-wrapper">
+            <p className="tm-card-caption">{card.attributes.name}</p>
+            <p className="tm-card-ddate">{toFineDate(new Date(card.attributes.ddate))}</p>
+            <div className="tm-card-distances">
+              {card.attributes.distances.data.toSorted((a, b) => {
+                return a.attributes.km < b.attributes.km ? -1 : 1
+              }).map(item => {
+                return <CourseSticker type={item.attributes.courseType} value={item.attributes.km}/>
+              })}
+            </div>
+          </div>
+          {card.attributes.status === raceStatus.closed && <div className="tm-card-reslink">Результаты</div>}
+          {card.attributes.status === raceStatus.opened && <div>список зарегистрированных</div>}
+          {!card.attributes.status && <div className="tm-card-disabled-block">скоро</div>}
         </div>
-      </div>
+      );
     })}
   </>);
 
